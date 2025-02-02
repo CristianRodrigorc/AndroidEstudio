432848e21f1b4ee99fc624dd13885ce1a698476b
CristianRodrigorc <cristian24121997@gmail.com>
2025-02-02 22:32:05 +0100

    Se terminó el Screen de Usuario, falta mejorar los métodos

--- AppGym/app/src/main/java/com/cristianproyecto/appgym/screens/UsersScreen.kt
+++ AppGym/app/src/main/java/com/cristianproyecto/appgym/screens/UsersScreen.kt
@@ -2,24 +2,34 @@ package com.cristianproyecto.appgym.screens
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.cristianproyecto.appgym.R
 import com.cristianproyecto.appgym.util.LectorJSON
+import com.cristianproyecto.appgym.util.UtilidadesBotones
 import com.cristianproyecto.appgym.util.UtilidadesUsersScreen
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class UsersScreen : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_users_screen)
 
+        val btnLunes = findViewById<Button>(R.id.btnLunes)
+        val btnMartes = findViewById<Button>(R.id.btnMartes)
+        val btnMiercoles = findViewById<Button>(R.id.btnMiercoles)
+        val btnJueves = findViewById<Button>(R.id.btnJueves)
+        val btnViernes = findViewById<Button>(R.id.btnViernes)
+        val btnSabado = findViewById<Button>(R.id.btnSabado)
+        val btnDomingo = findViewById<Button>(R.id.btnDomingo)
+        val btnVolverUserS = findViewById<Button>(R.id.btnVolverUserS)
 
         val tvEjercicio1 = findViewById<TextView>(R.id.tvEjercicio1)
         val swtEjercicio1 = findViewById<Switch>(R.id.swtEjercicio1)
@@ -56,12 +66,148 @@ class UsersScreen : AppCompatActivity() {
                 youtubeView)
         }
 
+        UtilidadesUsersScreen.cargarVideo(youtubeView,idVideoYTDefault)
 
+        btnLunes.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaLunes(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
 
+        btnMartes.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaMartes(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
 
+        btnMiercoles.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaMiercoles(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
 
+        btnJueves.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaJueves(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
 
+        btnViernes.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaViernes(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
 
-        UtilidadesUsersScreen.verVideo(youtubeView,idVideoYTDefault)
+        btnSabado.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaSabado(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
+
+        btnDomingo.setOnClickListener {
+            if (ejerciciosJson != null) {
+                UtilidadesUsersScreen.llenarDiaDomingo(
+                    ejerciciosJson,
+                    tvEjercicio1,
+                    descEjercicio1,
+                    swtEjercicio1,
+                    tvEjercicio2,
+                    descEjercicio2,
+                    swtEjercicio2,
+                    tvEjercicio3,
+                    descEjercicio3,
+                    swtEjercicio3,
+                    tvEjercicio4,
+                    descEjercicio4,
+                    swtEjercicio4,
+                    youtubeView)
+            }
+        }
+
+        UtilidadesBotones.cambiarScreen(btnVolverUserS,this,MainActivity::class.java)
     }
 }
--- AppGym/app/src/main/java/com/cristianproyecto/appgym/util/UtilidadesUsersScreen.kt
+++ AppGym/app/src/main/java/com/cristianproyecto/appgym/util/UtilidadesUsersScreen.kt
@@ -1,5 +1,6 @@
 package com.cristianproyecto.appgym.util
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.Switch
 import android.widget.TextView
@@ -8,7 +9,10 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
-object UtilidadesUsersScreen {
+@SuppressLint("StaticFieldLeak")
+object UtilidadesUsersScreen{
+    private var lastVisibleDesc: TextView? = null
+    private var youTubePlayer: YouTubePlayer? = null
 
     fun llenarDiaLunes(
         ejercicios: Ejercicios,
@@ -25,58 +29,29 @@ object UtilidadesUsersScreen {
         tvDescripcion4: TextView,
         switch4: Switch,
         youtubeView: YouTubePlayerView,
-    ){
-        ejercicios?.ejercicios?.biceps?.get(0).let {
-            if(it != null){
-                tvEjercicio1.text = it.nombre
-                tvDescripcion1.text = it.repeticiones
-            }
-            switch.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(1).let {
-            if(it != null){
-                tvEjercicio2.text = it.nombre
-                tvDescripcion2.text = it.repeticiones
-            }
-            switch2.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(2).let {
-            if(it != null){
-                tvEjercicio3.text = it.nombre
-                tvDescripcion3.text = it.repeticiones
-            }
-            switch3.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(3).let {
-            if(it != null){
-                tvEjercicio4.text = it.nombre
-                tvDescripcion4.text = it.repeticiones
-            }
-            switch4.isChecked = false
-        }
+    ) {
+        inicializarYoutubePlayer(youtubeView)
 
-        tvEjercicio1.setOnClickListener {
-            verDesc(tvDescripcion1)
-            val id = "JGoQA1ItmpM"
-            verVideo(youtubeView,id)
+        val ejerciciosBiceps = ejercicios.ejercicios.biceps
 
-        }
-        tvEjercicio2.setOnClickListener {
-            verDesc(tvDescripcion2)
-            val id = "0ajYejJTFVw"
-            verVideo(youtubeView,id)
-        }
-        tvEjercicio3.setOnClickListener {
-            verDesc(tvDescripcion3)
-            val id = "Wc9HsGgQ6_w"
-            verVideo(youtubeView,id)
-        }
-        tvEjercicio4.setOnClickListener {
-            verDesc(tvDescripcion4)
-            val id = "Is3JRhq37o4"
-            verVideo(youtubeView,id)
-        }
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "3k0Iu_ogVtw"),
+            Triple(tvEjercicio2, tvDescripcion2, "oq42eVowbD4"),
+            Triple(tvEjercicio3, tvDescripcion3, "lG53BKnQlxY"),
+            Triple(tvEjercicio4, tvDescripcion4, "Is3JRhq37o4")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
 
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
     }
 
     fun llenarDiaMartes(
@@ -94,62 +69,266 @@ object UtilidadesUsersScreen {
         tvDescripcion4: TextView,
         switch4: Switch,
         youtubeView: YouTubePlayerView,
-    ){
-        ejercicios?.ejercicios?.biceps?.get(0).let {
-            if(it != null){
-                tvEjercicio1.text = it.nombre
-                tvDescripcion1.text = it.repeticiones
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.triceps
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "aLtLLvffF6M"),
+            Triple(tvEjercicio2, tvDescripcion2, "vTdr4UKscRE"),
+            Triple(tvEjercicio3, tvDescripcion3, "gY-CqZD0Ktc"),
+            Triple(tvEjercicio4, tvDescripcion4, "uDjXYcXR0ys")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
             }
-            switch.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(1).let {
-            if(it != null){
-                tvEjercicio2.text = it.nombre
-                tvDescripcion2.text = it.repeticiones
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
             }
-            switch2.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(2).let {
-            if(it != null){
-                tvEjercicio3.text = it.nombre
-                tvDescripcion3.text = it.repeticiones
-            }
-            switch3.isChecked = false
-        }
-        ejercicios?.ejercicios?.biceps?.get(3).let {
-            if(it != null){
-                tvEjercicio4.text = it.nombre
-                tvDescripcion4.text = it.repeticiones
-            }
-            switch4.isChecked = false
         }
     }
 
-    fun llenarDiaMiercoles(){
 
+
+    fun llenarDiaMiercoles(
+        ejercicios: Ejercicios,
+        tvEjercicio1: TextView,
+        tvDescripcion1: TextView,
+        switch: Switch,
+        tvEjercicio2: TextView,
+        tvDescripcion2: TextView,
+        switch2: Switch,
+        tvEjercicio3: TextView,
+        tvDescripcion3: TextView,
+        switch3: Switch,
+        tvEjercicio4: TextView,
+        tvDescripcion4: TextView,
+        switch4: Switch,
+        youtubeView: YouTubePlayerView,
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.pecho
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "8LR0mo8iD8s"),
+            Triple(tvEjercicio2, tvDescripcion2, "vw_pcm2ly5Y"),
+            Triple(tvEjercicio3, tvDescripcion3, "ItBASjwB_Wo"),
+            Triple(tvEjercicio4, tvDescripcion4, "SnNyF9g8dDE")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
     }
-    fun llenarDiaJueves(){
 
+    fun llenarDiaJueves(
+        ejercicios: Ejercicios,
+        tvEjercicio1: TextView,
+        tvDescripcion1: TextView,
+        switch: Switch,
+        tvEjercicio2: TextView,
+        tvDescripcion2: TextView,
+        switch2: Switch,
+        tvEjercicio3: TextView,
+        tvDescripcion3: TextView,
+        switch3: Switch,
+        tvEjercicio4: TextView,
+        tvDescripcion4: TextView,
+        switch4: Switch,
+        youtubeView: YouTubePlayerView,
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.espalda
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "Vg1nmlJzGgM"),
+            Triple(tvEjercicio2, tvDescripcion2, "31vdmfx5pJs"),
+            Triple(tvEjercicio3, tvDescripcion3, "JddDALFiRbw"),
+            Triple(tvEjercicio4, tvDescripcion4, "SCsH7Z7qDwU")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
     }
-    fun llenarDiaViernes(){
 
+
+    fun llenarDiaViernes(
+        ejercicios: Ejercicios,
+        tvEjercicio1: TextView,
+        tvDescripcion1: TextView,
+        switch: Switch,
+        tvEjercicio2: TextView,
+        tvDescripcion2: TextView,
+        switch2: Switch,
+        tvEjercicio3: TextView,
+        tvDescripcion3: TextView,
+        switch3: Switch,
+        tvEjercicio4: TextView,
+        tvDescripcion4: TextView,
+        switch4: Switch,
+        youtubeView: YouTubePlayerView,
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.piernas
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "ozCH5r2lP2E"),
+            Triple(tvEjercicio2, tvDescripcion2, "MetkFq2hMZs"),
+            Triple(tvEjercicio3, tvDescripcion3, "FyWvCXvCC-w"),
+            Triple(tvEjercicio4, tvDescripcion4, "Pe9fw_B-B34")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
     }
-    fun llenarDiaSabado(){
 
+
+    fun llenarDiaSabado(
+        ejercicios: Ejercicios,
+        tvEjercicio1: TextView,
+        tvDescripcion1: TextView,
+        switch: Switch,
+        tvEjercicio2: TextView,
+        tvDescripcion2: TextView,
+        switch2: Switch,
+        tvEjercicio3: TextView,
+        tvDescripcion3: TextView,
+        switch3: Switch,
+        tvEjercicio4: TextView,
+        tvDescripcion4: TextView,
+        switch4: Switch,
+        youtubeView: YouTubePlayerView,
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.hombros
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "Xaa6rn3Hrh4"),
+            Triple(tvEjercicio2, tvDescripcion2, "DAMw-xGYNck"),
+            Triple(tvEjercicio3, tvDescripcion3, "9V8eNF-fkls"),
+            Triple(tvEjercicio4, tvDescripcion4, "bheSKL7AhGY")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
     }
-    fun llenarDiaDomingo(){
 
+
+    fun llenarDiaDomingo(
+        ejercicios: Ejercicios,
+        tvEjercicio1: TextView,
+        tvDescripcion1: TextView,
+        switch: Switch,
+        tvEjercicio2: TextView,
+        tvDescripcion2: TextView,
+        switch2: Switch,
+        tvEjercicio3: TextView,
+        tvDescripcion3: TextView,
+        switch3: Switch,
+        tvEjercicio4: TextView,
+        tvDescripcion4: TextView,
+        switch4: Switch,
+        youtubeView: YouTubePlayerView,
+    ) {
+        inicializarYoutubePlayer(youtubeView)
+
+        val ejerciciosBiceps = ejercicios.ejercicios.abdomen
+
+        listOf(
+            Triple(tvEjercicio1, tvDescripcion1, "QMvyEWQrmsY"),
+            Triple(tvEjercicio2, tvDescripcion2, "2ypB_CmVILM"),
+            Triple(tvEjercicio3, tvDescripcion3, "P60uxBkcaNI"),
+            Triple(tvEjercicio4, tvDescripcion4, "rMznoDrT5aI")
+        ).forEachIndexed { index, (tvEjercicio, tvDescripcion, videoId) ->
+            ejerciciosBiceps.getOrNull(index)?.let { ejercicio ->
+                tvEjercicio.text = ejercicio.nombre
+                tvDescripcion.text = ejercicio.repeticiones
+            }
+            tvDescripcion.visibility = View.GONE // Oculta las descripciones por defecto
+            listOf(switch, switch2, switch3, switch4)[index].isChecked = false
+
+            tvEjercicio.setOnClickListener {
+                verDesc(tvDescripcion)
+                cargarVideo(videoId)
+            }
+        }
+    }
+
+    private fun inicializarYoutubePlayer(youtubeView: YouTubePlayerView) {
+        if (youTubePlayer == null) {
+            youtubeView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady(player: YouTubePlayer) {
+                    youTubePlayer = player
+                }
+            })
+        }
     }
 
     private fun verDesc(descView: TextView) {
+        if (lastVisibleDesc != null && lastVisibleDesc != descView) {
+            lastVisibleDesc?.visibility = View.GONE
+        }
+
+
         if (descView.visibility == View.GONE) {
             descView.visibility = View.VISIBLE
+            lastVisibleDesc = descView
         } else {
             descView.visibility = View.GONE
+            lastVisibleDesc = null
         }
     }
 
-    fun verVideo(youtubeView: YouTubePlayerView, id: String){
+    private fun cargarVideo(videoId: String) {
+        youTubePlayer?.loadVideo(videoId, 0f)
+    }
+
+    fun cargarVideo(youtubeView: YouTubePlayerView, id: String){
         youtubeView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 //youTubePlayer.mute()
