81925ef9ca2020ea00d1c6c63a1ddea03f5a7cad
CristianRodrigorc <cristian24121997@gmail.com>
2025-08-11 22:10:31 +0200

    se limpió los proyectos vacíos

--- AppGym/app/src/main/java/com/cristianproyecto/appgym/model/Ejercicio.kt
+++
@@ -1,7 +0,0 @@
-package com.cristianproyecto.appgym.model
-
-
-data class Ejercicio(
-    val nombre: String,
-    val repeticiones: String
-)
--- AppGym/app/src/main/java/com/cristianproyecto/appgym/model/Ejercicios.kt
+++
@@ -1,6 +0,0 @@
-package com.cristianproyecto.appgym.model
-
-
-data class Ejercicios(
-    val ejercicios: EjerciciosCategorias
-)
